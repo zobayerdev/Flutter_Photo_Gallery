0acd3ec3f485dc7bf11bd6cc8b1f29b415e95173
zobayerdev <zobayer.dev@gmail.com>
2024-04-29 13:21:16 +0600

    file upload

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gallary/screens/home.dart';
+import 'package:device_preview/device_preview.dart';
+
+void main() => runApp(
+      DevicePreview(
+        enabled: true,
+        tools: const [...DevicePreview.defaultTools],
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Gallery - Ostad',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
